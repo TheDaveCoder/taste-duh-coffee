f1bdeea4df734aa7b6b1ce799d48121312e88b0a
dave <dave@DESKTOP-1BJ264U>
2024-03-04 15:25:03 +0800

    Updated productID property to itemID

--- src/Models/Sale.java
+++ src/Models/Sale.java
@@ -2,15 +2,15 @@ package Models;
 
 public class Sale {
     private int invoiceID;
-    private int productID;
+    private int itemID;
     private String sizeName;
     private Double unitPrice;
     private int quantity;
     private Double amount;
 
-    public Sale(int invoiceID, int productID, String sizeName, Double unitPrice, int quantity, Double amount) {
+    public Sale(int invoiceID, int itemID, String sizeName, Double unitPrice, int quantity, Double amount) {
         this.invoiceID = invoiceID;
-        this.productID = productID;
+        this.itemID = itemID;
         this.sizeName = sizeName;
         this.unitPrice = unitPrice;
         this.quantity = quantity;
@@ -20,8 +20,8 @@ public class Sale {
     public int getInvoiceID() {
         return invoiceID;
     }
-    public int getProductID() {
-        return productID;
+    public int getitemID() {
+        return itemID;
     }
     public String getSizeName() {
         return sizeName;
