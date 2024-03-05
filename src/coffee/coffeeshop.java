f2ee9000e6afc9518514492b2f36f3bda7d7f682
dave <dave@DESKTOP-1BJ264U>
2024-03-05 12:36:46 +0800

    Fixed the issue with the Reset button

--- src/coffee/coffeeshop.java
+++ src/coffee/coffeeshop.java
@@ -101,8 +101,9 @@ public class coffeeshop extends javax.swing.JFrame {
         total = 0.0;
         change = 0.0;
         
-        sales.removeAll(sales);
-        orderListTbl.removeAll();
+        sales.clear();
+        DefaultTableModel model = (DefaultTableModel) orderListTbl.getModel();
+        model.setRowCount(0);
         invoiceTextArea.setText("");
     }
     
@@ -2054,8 +2055,7 @@ public class coffeeshop extends javax.swing.JFrame {
         if(response == JOptionPane.YES_OPTION) {
             resetToZero(); 
         }              
-    }//GEN-LAST:event_jButtonResetActionPerformed
-
+    }//GEN-LAST:event_jActionPerformed
     private void americanoPriceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_americanoPriceActionPerformed
      
     }//GEN-LAST:event_americanoPriceActionPerformed
